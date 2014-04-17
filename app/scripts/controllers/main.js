cdfc5cf82536992586e623b878e32c23c2025fc4
ZJR <me@zjr.io>
2014-04-17 02:21:31 -0400

    Cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,28 +1,5 @@
 'use strict';
 
 angular.module('marvelPediaApp')
-  .controller('MainCtrl', function ($scope, $http, MarvelService) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    $scope.marvel = MarvelService;
-
-    var apikey = '2b3213d7c8dd78f64c05f712cfbfa4ff';
-    var version = '/v1/public';
-    var baseUrl = 'http://gateway.marvel.com' + version + '/characters' + '?limit=100&apikey=' + apikey;
-
-    $http({
-      method: 'GET',
-      url: baseUrl
-    })
-    .success(function() {
-      $scope.result = arguments;
-    })
-    .error(function() {
-      $scope.result = arguments;
-    });
-
+  .controller('MainCtrl', function ($scope) {
   });
