93a27597283597922c1160bbd7020d35610c2b66
ZJR <me@zjr.io>
2014-04-17 00:10:10 -0400

    Add http request to controller cause blah.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('marvelPediaApp')
-  .controller('MainCtrl', function ($scope, MarvelService) {
+  .controller('MainCtrl', function ($scope, $http, MarvelService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -9,4 +9,20 @@ angular.module('marvelPediaApp')
     ];
 
     $scope.marvel = MarvelService;
+
+    var apikey = '2b3213d7c8dd78f64c05f712cfbfa4ff';
+    var version = '/v1/public';
+    var baseUrl = 'http://gateway.marvel.com' + version + '/characters' + '?limit=100&apikey=' + apikey;
+
+    $http({
+      method: 'GET',
+      url: baseUrl
+    })
+      .success(function() {
+        $scope.result = arguments;
+      })
+      .error(function() {
+        $scope.result = arguments;
+      });
+
   });
